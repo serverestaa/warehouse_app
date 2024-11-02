2c298fa1cad591019061223f572755b2e7f8934d
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-11-02 14:14:12 +0500

    just a practice with redis

---
+++ store/main.py
@@ -0,0 +1,93 @@
+import time
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+import requests
+from fastapi.background import BackgroundTasks
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+redis = get_redis_connection(
+    host='redis-11115.c283.us-east-1-4.ec2.redns.redis-cloud.com',
+    port="11115",
+    password='iYsOZ5QcmDsBTvfwOP8x2PJa7OBZzh9w',
+    decode_responses=True
+)
+
+
+class ProductOrder(HashModel):
+    product_id: str
+    quantity: int
+
+    class Meta:
+        database = redis
+
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str
+
+    class Meta:
+        database = redis
+
+
+@app.post("/orders", tags=['store'])
+def create(productOrder: ProductOrder, background_tasks: BackgroundTasks):
+    req = requests.get(f'http://localhost:8000/product/{productOrder.product_id}')
+    product = req.json()
+    fee = product['price'] * 0.2
+
+    order = Order(
+        product_id=productOrder.product_id,
+        price=product['price'],
+        fee=fee,
+        total=product['price'] + fee,
+        quantity=productOrder.quantity,
+        status='pending'
+    )
+
+    order.save()
+
+    background_tasks.add_task(order_complete, order)
+
+    return order
+
+
+@app.get('/orders/{pk}', tags=['store'])
+def get(pk: str):
+    return format(pk)
+
+
+@app.get('/orders', tags=['store'])
+def get_all():
+    return [format(pk) for pk in Order.all_pks()]
+
+
+def format(pk: str):
+    order = Order.get(pk)
+    return {
+        'id': order.pk,
+        'product_id': order.product_id,
+        'fee': order.fee,
+        'total': order.total,
+        'quantity': order.quantity,
+        'status': order.status
+    }
+
+
+def order_complete(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
+    redis.xadd(name='order-completed', fields=order.dict())
